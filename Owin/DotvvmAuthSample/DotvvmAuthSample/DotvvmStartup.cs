9d59b0f61f8b8df61b50f418b90b395082e0c4f9
Patrik Švikruha <patrik.svikruha@riganti.cz>
2018-11-08 19:13:35 +0100

    update sample to dotvvm 2.0.3

--- Owin/DotvvmAuthSample/DotvvmAuthSample/DotvvmStartup.cs
+++ Owin/DotvvmAuthSample/DotvvmAuthSample/DotvvmStartup.cs
@@ -1,16 +1,15 @@
-using System.Web.Hosting;
-using Microsoft.Owin;
-using Microsoft.Owin.FileSystems;
-using Microsoft.Owin.StaticFiles;
-using Owin;
-using DotVVM.Framework;
 using DotVVM.Framework.Configuration;
-using DotVVM.Framework.Routing;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace DotvvmAuthSample
 {
-    public class DotvvmStartup : IDotvvmStartup
+    public class DotvvmStartup : IDotvvmStartup, IDotvvmServiceConfigurator
     {
+        public void ConfigureServices(IDotvvmServiceCollection options)
+        {
+            options.AddDefaultTempStorages("temp");
+        }
+
         // For more information about this class, visit https://dotvvm.com/docs/tutorials/basics-project-structure
         public void Configure(DotvvmConfiguration config, string applicationPath)
         {
